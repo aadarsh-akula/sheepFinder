bbf6e401080fdff04f22b721fd6d32a96a39abe8
Yor Her <yor_her@hotmail.com>
2021-10-08 23:18:04 -0500

    Did some changes and set up
    Will need to fix more things, this is just so that it's set up.

---
+++ sheepfinder/src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState(""); // catch if detail are correct
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details do not match!");
+      setError("incorrect email or password");
+    }
+  };
+
+  const Logout = () => {
+    setUser({
+      name: "",
+      email: "",
+    });
+  };
+
+  return (
+    <div className="App">
+      {user.email != "" ? ( //if user and email does not match then we render welcome screen
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ sheepfinder/src/components/LoginForm.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./index.css";
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({ name: "", email: "", password: "" });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    Login(details); //pass in details
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="error-message">
+        {error != "" ? <div className="error">{error}</div> : ""}
+      </div>
+      <div className="sheep-body">
+        {" "}
+        <h1 className="welcome-text">Welcome to SheepFinder</h1>
+        <h1 className="welcome-text">Login</h1>
+        <div>
+          <img src="https://cdn1.iconfinder.com/data/icons/follow-the-herd/512/blank_left-128.png" />
+        </div>
+        <div className="middle-sheep-body">
+          {/* <div>
+            <p className="asterik">Name</p>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={(e) => setDetails({ ...details, name: e.target.value })}
+              value={details.name}
+            />
+         </div> */}
+
+          <div>
+            <p className="asterik">Email</p>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              onChange={(e) =>
+                setDetails({ ...details, email: e.target.value })
+              }
+              value={details.email}
+            />
+          </div>
+
+          <div>
+            <p className="asterik">Password</p>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) =>
+                setDetails({ ...details, password: e.target.value })
+              }
+              value={details.password}
+            />
+          </div>
+          <input className="login" type="submit" value="Login" />
+        </div>{" "}
+        <footer>&copy;Copyright 2021</footer>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
