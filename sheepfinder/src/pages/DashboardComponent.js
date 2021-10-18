e8c510e1d227594ac87055c55c5b1bc67b50144b
HERY2507 <45435134+HERY2507@users.noreply.github.com>
2021-10-17 21:49:04 -0500

    change dashboard
    ...

--- sheepfinder/src/pages/DashboardComponent.js
+++ sheepfinder/src/pages/DashboardComponent.js
@@ -1,31 +1,61 @@
+import { Link } from "react-router-dom";
+
 function DashboardComponent(props) {
   const jobList = [
     { description: "Bus Driver", key: 0 },
     { description: "Weed Seller", key: 1 },
     { description: "Cocaine Dealer", key: 2 },
     { description: "..............................", key: 3 },
-    { description: "..............................", key: 4 },
-    { description: "..............................", key: 5 },
-    { description: "..............................", key: 6 },
-    { description: "..............................", key: 7 },
-    { description: "..............................", key: 8 },
-    { description: "..............................", key: 9 },
-    { description: "..............................", key: 10 },
+
   ];
   return (
     <div className="dashboard_component">
-      <div>
+      <div className="dashboard_jobbox">
         <h2>Job List</h2>
         <div className="joblist">
-    
-          {
-            <ol>
-              {jobList.map((joblist) => (
-                <li key={joblist.key}>{joblist.description}</li>
-              ))}
-            </ol>
-          }
+          <div>
+          <ol>
+          <li>
+          Bus Driver
+          </li>
+          <li>
+          Weed Seller
+          </li>
+          <li>
+          Cocaine Dealer
+          </li>
+          <li>
+          .............................
+          </li>
+          </ol>
+          </div>
+
+          <div>
+          <ol className="apply_button_ol">
+          <li>
+          <Link className="button_apply" to="/dashboard" value="Login">
+            Apply
+          </Link>
+          </li>
+          <li>
+          <Link className="button_apply" to="/dashboard" value="Login">
+            Apply
+          </Link>
+          </li>
+          <li>
+          <Link className="button_apply" to="/dashboard" value="Login">
+            Apply
+          </Link>
+          </li>
+          <li>
+          <Link className="button_apply" to="/dashboard" value="Login">
+            Apply
+          </Link>
+          </li>
+          </ol>
+          </div>
         </div>
+        
       </div>
     </div>
   );
